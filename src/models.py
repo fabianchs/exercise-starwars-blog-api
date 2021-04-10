adb2cf640b15e7c93e561ded842b0b1a315ebd9f
fabianchs <fabichasola@hotmail.com>
2021-04-10 01:15:55 +0000

    f

--- src/models.py
+++ src/models.py
@@ -20,6 +20,17 @@ class User(db.Model):
     username = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(250), nullable=False)
 
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.username,
+            # do not serialize the password, its a security breach
+        }
+
 class FavPeople(db.Model):
     __tablename__ = 'FavPeople'
     # Here we define columns for the table person
@@ -83,19 +94,3 @@ class Planets(db.Model):
     
 
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
