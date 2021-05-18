587642098b394bb7eb0dba6d7d60f4b53cb126df
fabianchs <fabichasola@hotmail.com>
2021-05-18 14:30:36 +0000

    working on favs

--- src/models.py
+++ src/models.py
@@ -107,7 +107,7 @@ class FavsUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fav_name= db.Column(db.String(120), unique=False, nullable=False)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship(User, foreign_keys=[id_user])
+
 
 
     def __repr__(self):
